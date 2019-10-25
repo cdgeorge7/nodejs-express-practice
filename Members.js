eaeb7de02691561743cb94d168409b6b77557f37
cdgeorge7 <cdgeorge7@gmail.com>
2019-10-25 00:19:33 -0500

    Simple CRUD API

---
+++ Members.js
@@ -0,0 +1,11 @@
+const members = [
+  { id: 1, name: "Tony the Tiger", email: "TT@tiger.com", status: "active" },
+  {
+    id: 2,
+    name: "Bill the Balloon",
+    email: "BB@balloon.com",
+    status: "inactive"
+  }
+];
+
+module.exports = members;
